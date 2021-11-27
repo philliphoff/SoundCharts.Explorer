0d6d6c92ca59b22bdcf768c237ae90bfddd3c914
Phillip Hoff <phillip@orst.edu>
2021-11-26 23:54:38 -0800

    Scaffold map view.

--- src/SoundCharts.Explorer.MacOS/ViewController.designer.cs
+++ src/SoundCharts.Explorer.MacOS/ViewController.designer.cs
@@ -1,19 +1,26 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace SoundCharts.Explorer.MacOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		MapKit.MKMapView mapView { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (mapView != null) {
+				mapView.Dispose ();
+				mapView = null;
+			}
 		}
 	}
 }
-
