8e5c026a0b0d11101e5fb62bb3922487348d498b
Phillip Hoff <phillip@orst.edu>
2021-11-26 23:01:34 -0800

    Initial commit.

---
+++ SoundCharts.Explorer.MacOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace SoundCharts.Explorer.MacOS
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
