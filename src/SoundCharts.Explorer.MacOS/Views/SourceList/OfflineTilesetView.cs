3b97a6b11fb7b7406fe58d20ccba57880db707fb
Phillip Hoff <phillip@orst.edu>
2022-02-22 23:32:41 -0800

    Show action on hover.

--- src/SoundCharts.Explorer.MacOS/Views/SourceList/OfflineTilesetView.cs
+++ src/SoundCharts.Explorer.MacOS/Views/SourceList/OfflineTilesetView.cs
@@ -15,6 +15,17 @@ namespace SoundCharts.Explorer.MacOS.Views.SourceList
         {
         }
 
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            this.ActionButton.Hidden = true;
+
+            var area = new NSTrackingArea(this.Bounds, NSTrackingAreaOptions.ActiveAlways | NSTrackingAreaOptions.MouseEnteredAndExited, this, null);
+
+            this.AddTrackingArea(area);
+        }
+
         public void Initialize(string title, string description, Func<Task> onAction)
         {
             this.TitleTextField.StringValue = title;
@@ -25,8 +36,21 @@ namespace SoundCharts.Explorer.MacOS.Views.SourceList
 
         partial void OnActionButtonAction(NSObject sender)
         {
-            
             this.onAction?.Invoke();
+        }
+
+        public override void MouseEntered(NSEvent theEvent)
+        {
+            this.ActionButton.Hidden = false;
+
+            base.MouseEntered(theEvent);
+        }
+
+        public override void MouseExited(NSEvent theEvent)
+        {
+            this.ActionButton.Hidden = true;
+
+            base.MouseExited(theEvent);
         }
     }
 }
