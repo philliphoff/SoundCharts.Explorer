ed05ef6540bf81553e8812d387f0a3271551c768
Phillip Hoff <phillip@orst.edu>
2022-02-23 23:58:58 -0800

    Revamp toolbar.

--- src/SoundCharts.Explorer.MacOS/AppViewController.cs
+++ src/SoundCharts.Explorer.MacOS/AppViewController.cs
@@ -1,16 +1,24 @@
-﻿using System;
+using System;
 using AppKit;
 using Foundation;
 
 namespace SoundCharts.Explorer.MacOS
 {
 	[Register("AppViewController")]
-	public class AppViewController : NSWindowController
+	public partial class AppViewController : NSWindowController
 	{
 		public AppViewController(IntPtr handle)
 			: base(handle)
 		{
 		}
+
+		partial void OnToggleSidebar(NSObject sender)
+        {
+			if (this.ContentViewController is NSSplitViewController splitViewController)
+            {
+				splitViewController.ToggleSidebar(sender);
+            }
+        }
 	}
 }
 
